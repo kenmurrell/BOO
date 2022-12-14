f980a1c6ea10f43933e504dfe933ef700e2edabe
Jonathan Papineau <jonathan.papineau@magnetforensics.com>
2022-12-14 16:01:16 -0500

    create a simple chunk tree cache

---
+++ BtrfsObjectOpener/ChunkTreeCache.cs
@@ -0,0 +1,27 @@
+﻿namespace BtrfsObjectOpener;
+
+public record ChunkKey(ulong start, ulong size);
+
+public record ChunkValue(ulong offset);
+
+public class ChunkTreeCache
+{
+    private List<(ChunkKey, ChunkValue)> cache = new();
+    
+    public void Insert(ChunkKey key, ChunkValue value) {
+        cache.Add((key, value));
+    }
+
+    public ulong? GetPhysicalOffset(ulong logical)
+    {
+        foreach (var (k, v) in cache)
+        {
+            if (logical >= k.start && logical < k.start + k.size)
+            {
+                return v.offset + logical - k.start;
+            }
+        }
+
+        return null;
+    }
+}
