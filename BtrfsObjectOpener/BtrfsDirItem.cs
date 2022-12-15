f33599b0bc67d87b9f149cc47801fd95eca13ec1
Jonathan Papineau <jonathan.papineau@magnetforensics.com>
2022-12-15 15:25:50 -0500

    Add BtrfsDirItem

---
+++ BtrfsObjectOpener/BtrfsDirItem.cs
@@ -0,0 +1,36 @@
+﻿namespace BtrfsObjectOpener;
+
+
+/// Reference: https://btrfs.wiki.kernel.org/index.php/Data_Structures#btrfs_dir_item
+public class BtrfsDirItem : DiskObject
+{
+    public int Size => 30;
+
+    public BtrfsDiskKey location;
+
+    public ulong transid;
+
+    public ushort data_len;
+
+    public ushort name_len;
+
+    public byte type;
+    
+    
+    public int ReadFrom(byte[] data, int offset)
+    {
+        if(Size > data.Length - offset)
+        {
+            return 0;
+        }
+
+        location = new BtrfsDiskKey();
+        location.ReadFrom(data, offset);
+        transid = BitConverter.ToUInt64(data, offset + 17);
+        data_len = BitConverter.ToUInt16(data, offset + 25);
+        name_len = BitConverter.ToUInt16(data, offset + 25);
+        type = data[offset + 29];
+            
+        return Size;
+    }
+}
